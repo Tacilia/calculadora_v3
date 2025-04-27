8648564993c1d62242e912172693e1dd6047e71f
Tacilia <taciliacristina28@gmail.com>
2025-04-27 20:28:11 -0300

    inserindo calculadora_api em um  banco de dados

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"log"           // usado para registrar erros e parar o programa se algo der errado.
 	"net/http"      //cria o servidor web e lida com as rotas da API.
 
-	_ "github.com/mattn/go-sqlite3" //  importa o driver do banco SQLite, o _ é porque só queremos registrar o driver, sem usar diretamente.
+	_ "github.com/mattn/go-sqlite3" // importa o driver do banco SQLite, o _ é porque só queremos registrar o driver, sem usar diretamente.
 	//Tambbém instalei a dependencia via terminal: go get github.com/mattn/go-sqlite3
 	//permite que a linguagem Go consiga se conectar e fazer consultas em um banco de dados SQLite.
 )
@@ -34,17 +34,21 @@ type HistoricoItem struct {
 	Resultado float64 `json:"resultado"`
 }
 
-var db *sql.DB // Variável global para o banco de dados
+var db *sql.DB // Criar uma variável global db que vai armazenar a conexão aberta com o banco de dados."
 
 // Funcao principal que inicia o Servidor
 func main() {
 	// variavel err que cria o banco de dados SQLite
 	var err error
-	db, err = sql.Open("sqlite3", "./calculadora.db")
+	db, err = sql.Open("sqlite3", "./calculadora.db") //Configura o acesso para quando vocêo cliente fizer a primeira operação.
+	// Se o arquivo calculadora.db ainda não existir, o driver do SQLite automaticamente cria esse arquivo na hora da primeira operação.
+
+	//Se der erro, ele é guardado na variável err criado na linha 41.
 	if err != nil {
 		log.Fatal("Erro ao abrir o banco:", err)
+		//Se hoouver erro ao abrir o banco, para o programa e mostra o erro.
 	}
-	defer db.Close()
+	defer db.Close() //Garante que o banco de dados seja encerrado quando o programa terminar.
 
 	http.HandleFunc("/soma", somaHandler) //rota de cada api
 	http.HandleFunc("/subtracao", subtracaoHandler)
